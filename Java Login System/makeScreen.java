b5f885bd2828df5e3b382be03fc531fc0da22416
123Rocco123 <petruccio.rocco@gmail.com>
2022-02-23 00:24:17 +0000

    Username Label and JTextField

--- Java Login System/makeScreen.java
+++ Java Login System/makeScreen.java
@@ -2,18 +2,30 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-class makeScreen {
-  Frame newPanel;
+class makeScreen extends JFrame{
+  // It's important to note that we have to declare and initialize the instance variables as soon as the class is made.
+     // This is because of the fact that if we don't, then they won't get rendered into the panel when the program runs until we resize the frame. 
+  JTextField username = new JTextField(30);
+  JTextField password;
 
   public makeScreen() {
-    this.newPanel = new Frame("Test");
-    this.newPanel.setVisible(true);
+    setTitle("Test");
+    setVisible(true);
+    // Width * Height
+    setSize(400, 400);
+
+    // The panel below is used to store the username.
+    Panel userNamePanel = new Panel();
+    JLabel userNameInfo = new JLabel("Enter Username:\n");
+
+    userNamePanel.add(userNameInfo);
+    userNamePanel.add(this.username);
+
+    this.add(userNamePanel);
   }
 
   public void screenProperties() {
-    this.newPanel.setSize(400, 400);
-
-    newPanel.addWindowListener (new WindowAdapter() {
+    this.addWindowListener (new WindowAdapter() {
       public void windowClosing(WindowEvent e) {
         System.exit(0);
       }
