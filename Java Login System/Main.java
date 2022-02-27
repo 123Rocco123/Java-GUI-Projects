ef18b6cddf905f66e03b7351911902e54cb5e2de
123Rocco123 <petruccio.rocco@gmail.com>
2022-02-27 19:37:28 +0000

    Main static variables

--- Java Login System/Main.java
+++ Java Login System/Main.java
@@ -1,9 +1,12 @@
 import java.awt.*;
 
 class Main {
+  static String userName;
+  static String password;
+
   public static void main(String[] args) {
     homeScreen home = new homeScreen();
-    
+
     home.screenProperties();
   }
 }
