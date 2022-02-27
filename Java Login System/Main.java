115dda505afaa72ccab01f0c96a8295d43754efb
123Rocco123 <petruccio.rocco@gmail.com>
2022-02-27 02:38:42 +0000

    Login Screen Changes

--- Java Login System/Main.java
+++ Java Login System/Main.java
@@ -3,9 +3,7 @@ import java.awt.*;
 class Main {
   public static void main(String[] args) {
     homeScreen home = new homeScreen();
-    makeScreen newScreen = new makeScreen();
-
-    newScreen.screenProperties();
+    
     home.screenProperties();
   }
 }
