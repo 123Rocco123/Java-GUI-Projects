305326edd3b306d0c5161c3421d9a2e382bbb822
123Rocco123 <petruccio.rocco@gmail.com>
2022-02-28 16:03:54 +0000

    JTextArea

--- Java TextEditor/homeScreen.java
+++ Java TextEditor/homeScreen.java
@@ -1,6 +1,26 @@
-import java.awl.*;
+import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
-class homeScreen {
+class homeScreen extends JFrame {
+  Panel textEditorArea = new Panel();
 
+  JButton submitButton = new JButton("Submit");
+  JLabel Information = new JLabel("Write the text below:");
+  JTextArea textEditor = new JTextArea(30, 30);
+
+  public homeScreen() {
+    this.setSize(400,580);
+    this.setLocation(750, 250);
+    this.setVisible(true);
+    this.setTitle("Rocco's Text Editor");
+
+    textEditorArea.add(Information);
+    textEditorArea.add(textEditor);
+    textEditorArea.add(submitButton);
+    this.add(textEditorArea);
+
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 }
