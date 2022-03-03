bf7d30214d52fea508adfe5668b85bbc6f448fb4
123Rocco123 <petruccio.rocco@gmail.com>
2022-03-03 18:18:02 +0000

    JTextArea and openExistingFile Action Listener

--- Java TextEditor/Login.java
+++ Java TextEditor/Login.java
@@ -7,6 +7,7 @@ class Login extends JFrame {
   JPanel ButtonsPanel = new JPanel();
   JButton newFile = new JButton("New File");
   JButton openExistingFile = new JButton("Open File");
+  JTextArea information = new JTextArea("Hello,\n\nMy name is Rocco Petruccio, and this is the first \"app\" \nwhich I've created.\n\nI hope that you enjoy it.", 10, 30);
 
   public Login() {
     this.setSize(400,400);
@@ -14,9 +15,21 @@ class Login extends JFrame {
     this.setVisible(true);
     this.setTitle("Home Screen");
 
+    ButtonsPanel.add(information);
     ButtonsPanel.add(newFile);
+    ButtonsPanel.add(openExistingFile);
     this.add(ButtonsPanel);
 
+    // The code below is used to dipose of the current window, when the "Open File" button is pressed.
+    openExistingFile.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        dispose();
+        homeScreen mainScreen = new homeScreen();
+        mainScreen.whatOpened = "existing";
+      }
+    });
+
+    // The code below is used to dipose of the current window, when the "New File" button is pressed.
     newFile.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         dispose();
