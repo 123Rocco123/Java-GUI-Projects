91b754c3f8d2eded1624fdacddb2b98281782310
123Rocco123 <petruccio.rocco@gmail.com>
2022-04-07 05:06:03 +0100

    Instructions Class Layout

--- Java Adventure Game/instructions.java
+++ Java Adventure Game/instructions.java
@@ -2,8 +2,17 @@ import javax.swing.*;
 
 import java.awt.event.*;
 
-class instructions extends JFrame{
+class instructions extends JFrame {
+  JTextArea information = new JTextArea();
+
+  JPanel informationPanel = new JPanel();
+
   public instructions() {
-    
+    this.setLocation(750, 200);
+    this.setSize(300, 300);
+    this.setLayout(false);
+
+    informationPanel.setLayout(null);
+    informationPanel.setBounds(10, 10, 280, 280);
   }
 }
