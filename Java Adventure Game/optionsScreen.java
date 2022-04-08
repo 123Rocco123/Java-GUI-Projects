a2ecaf3cadf6eb71bc49f1a6b958febb83eb1859
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-04-08 22:32:58 +0100

    JComboBox ActionListener

--- Java Adventure Game/optionsScreen.java
+++ Java Adventure Game/optionsScreen.java
@@ -4,22 +4,20 @@ import java.awt.*;
 import java.awt.event.*;
 
 class optionsScreen extends JFrame {
-  static GraphicsDevice device = GraphicsEnvironment
-        .getLocalGraphicsEnvironment().getScreenDevices()[0];
-
   JLabel resolution = new JLabel("Screen Resolution");
   String[] resolutions = {"Programmers Choice", "Fullscreen"};
+  JComboBox<String> resolutionList = new JComboBox<String>(resolutions);
+
+  String resolution123;
 
   JPanel optionsPanel = new JPanel();
 
   public optionsScreen() {
-    final JFrame frame = new JFrame("Display Mode");
-
     this.setSize(300, 300);
     this.setLocation(750, 200);
     this.setLayout(null);
     this.add(optionsPanel);
-    this.setTitle("")
+    this.setTitle("Game Options");
 
     optionsPanel.setBounds(10, 10, 265, 240);
     optionsPanel.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -27,11 +25,16 @@ class optionsScreen extends JFrame {
     optionsPanel.add(resolution);
     resolution.setBounds(150, 10, 100, 25);
 
-    JComboBox<String> resolutionList = new JComboBox<String>(resolutions);
     resolutionList.setSelectedIndex(0);
     resolutionList.setBounds(150, 45, 100, 25);
     optionsPanel.add(resolutionList);
 
     this.setVisible(true);
+
+    resolutionList.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        resolution123 = (String)(resolutionList.getSelectedItem());
+      }
+    });
   }
 }
