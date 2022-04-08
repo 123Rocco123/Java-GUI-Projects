a3aa04c2997422d8d0d6e98b9f552b28e7e1fc97
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-04-08 22:28:56 +0100

    this to JFrame object

--- Java Adventure Game/game.java
+++ Java Adventure Game/game.java
@@ -4,19 +4,21 @@ import javax.swing.*;
 
 import java.awt.event.*;
 
-class game extends JFrame implements KeyListener {
+class game implements KeyListener {
+  JFrame frame = new JFrame("test");
+
   public game() {
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     // This makes it so that this JFrame will be able to listen to the keys on the keyboard.
-    this.addKeyListener(this);
+    frame.addKeyListener(this);
 
-    this.setSize(500, 500);
-    this.setResizable(false);
-    this.setLayout(null);
-    this.setLocation(750, 200);
+    frame.setSize(500, 500);
+    frame.setResizable(false);
+    frame.setLayout(null);
+    frame.setLocation(750, 200);
 
-    this.setVisible(true);
+    frame.setVisible(true);
   }
 
   @Override
