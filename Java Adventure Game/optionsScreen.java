10181d08abc9fddfd85c4e3ab65c1d09e7b31421
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-03 01:11:19 +0100

    Formatting and Button

--- Java Adventure Game/optionsScreen.java
+++ Java Adventure Game/optionsScreen.java
@@ -5,12 +5,14 @@ import java.awt.event.*;
 
 class optionsScreen extends JFrame {
   JLabel resolution = new JLabel("Screen Resolution");
-  String[] resolutions = {"Programmers Choice", "Fullscreen"};
+  String[] resolutions = {"Null", "Programmers Choice", "Fullscreen"};
   JComboBox<String> resolutionList = new JComboBox<String>(resolutions);
 
   String resolution123;
 
   JPanel optionsPanel = new JPanel();
+
+  JButton saveAndExit = new JButton("Save and Exit");
 
   public optionsScreen() {
     this.setSize(300, 300);
@@ -29,11 +31,20 @@ class optionsScreen extends JFrame {
     resolutionList.setBounds(150, 45, 100, 25);
     optionsPanel.add(resolutionList);
 
+    saveAndExit.setBounds(150, 200, 25, 25);
+    optionsPanel.add(saveAndExit);
+
     this.setVisible(true);
 
     resolutionList.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         resolution123 = (String)(resolutionList.getSelectedItem());
+      }
+    });
+
+    saveAndExit.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        dispose();
       }
     });
   }
