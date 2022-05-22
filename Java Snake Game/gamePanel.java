a6116e1c591e7e3febe26cc27849715641d368cc
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-22 02:39:09 +0100

    Instance Variables

--- Java Snake Game/gamePanel.java
+++ Java Snake Game/gamePanel.java
@@ -6,8 +6,35 @@ import javax.swing.*;
 import java.util.Random;
 
 class gamePanel extends JPanel implements ActionListener {
+  final int screenWidth = 600;
+  final int screenHeight = 600;
+
+  // Unit size of the snake and the food that it can eat.
+  final int unitSize = 25;
+  // Used to see how many objects we can fit onto the screen.
+  final int gameUnits = (screenWidth * screenHeight) / unitSize;
+  final int delay = 75;
+
+  // The reason that we create an empty pre-defined
+  final int[] snakeArrayX = new int[gameUnits];
+  final int[] snakeArrayY = new int[gameUnits];
+  int bodyParts = 6;
+
+  int foodEaten = 0;
+  int foodX;
+  int foodY;
+
+  char direction = "R";
+  boolean running = false;
   @Override
   public void actionPerformed(ActionEvent e) {
 
+  }
+
+  public class keyAdapter extends keyAdapter {
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
   }
 }
