85c804c9dc9c8be122166af276e197acf3cbe9fb
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-22 02:38:12 +0100

    JFrame Settings

--- Java Snake Game/GameFrame.java
+++ Java Snake Game/GameFrame.java
@@ -2,5 +2,15 @@ import javax.swing.*;
 
 class GameFrame extends JFrame {
   public GameFrame() {
+    // The same concept of not having to use the variable once the class instance has been created.
+    this.add(new gamePanel());
+    this.setTitle("Snake");
+    this.setDefaultCloseOperations(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    // Used to automatically size all of the components in the frame to their preferred size or larger.
+    this.pack();
+    this.setVisible(true);
+    // This will make it so that it appears at the center of our screen.
+    this.setLocationRelativeTo(null);
   }
 }
