e378fd414db1d80f86cbbe0cf2e49c04d945bca7
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-22 02:37:42 +0100

    Initial Commit

---
+++ Java Snake Game/main.java
@@ -0,0 +1,7 @@
+class main {
+  public static void main(String[] args) {
+    // Since we only need to start the game with the calling of the GameFrame class, we don't have to save it to a variable.
+       // Instead, we can just create a new instance of the class as seen below. 
+    new GameFrame();
+  }
+}
