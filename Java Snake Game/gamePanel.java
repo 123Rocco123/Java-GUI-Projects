e72b3f4117937418b81fb663d26d634f99642caa
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-23 03:42:02 +0100

    Key Listener Function

--- Java Snake Game/gamePanel.java
+++ Java Snake Game/gamePanel.java
@@ -165,7 +165,32 @@ class gamePanel extends JPanel implements ActionListener {
   class newKeyAdapter extends KeyAdapter {
     @Override
     public void keyPressed(KeyEvent e) {
+      // The reason that we have an if statement in the cases is because of the fact that we don't want to allow the user to go into themselves in a straight line. 
+      switch(e.getKeyCode()) {
+        case KeyEvent.VK_LEFT:
+          if (direction != 'R') {
+            direction = 'L';
+          }
+          break;
 
+        case KeyEvent.VK_RIGHT:
+          if (direction != 'L') {
+            direction = 'R';
+          }
+          break;
+
+        case KeyEvent.VK_UP:
+          if (direction != 'D') {
+            direction = 'U';
+          }
+          break;
+
+        case KeyEvent.VK_DOWN:
+          if (direction != 'U') {
+            direction = 'D';
+          }
+          break;
+      }
     }
   }
 }
