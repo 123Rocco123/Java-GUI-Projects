f2671f5978fd1c2b8e799a39c577531a7ab35f43
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-23 21:21:23 +0100

    JPanel Changes

--- Tic Tac Toe/gamepanel.java
+++ Tic Tac Toe/gamepanel.java
@@ -4,26 +4,22 @@ import java.awt.event.*;
 import javax.swing.*;
 
 class gamepanel extends JFrame {
-  JPanel gameScreen = new JPanel();
+  JPanel gameScreen = new panel();
+
+  String computerPiece = "";
 
   public gamepanel() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(500, 500);
+    this.setTitle("Tic Tac Toe");
 
     // Adding Panel to the Screen
     this.add(gameScreen);
-    gamePanel();
 
     // Size and Location of Frame
     this.pack();
     this.setLocationRelativeTo(null);
     this.setVisible(true);
-  }
-
-  // Panel Function
-  public void gamePanel() {
-    gameScreen.setBackground(Color.black);
-    gameScreen.setPreferredSize(new Dimension(500, 500));
   }
 
   public void gameAlgorithm(String playerChoice) {
