cccd38f1d2d37078596cf98ab1e4fecf5bf7e2a2
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-23 18:51:45 +0100

    Initial Commit

---
+++ Tic Tac Toe/gamepanel.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+class gamepanel extends JFrame {
+  JPanel gameScreen = new JPanel();
+  public gamepanel() {
+  }
+
+  // Panel Function
+  public void gamePanel() {
+  }
+
+  public void gameAlgorithm(String playerChoice) {
+    }
+  }
+}
