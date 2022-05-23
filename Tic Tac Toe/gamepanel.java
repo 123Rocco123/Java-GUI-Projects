7a0becc904d4b933e914e8eff292f4f86c22a555
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-23 18:51:55 +0100

    Main Functions

--- Tic Tac Toe/gamepanel.java
+++ Tic Tac Toe/gamepanel.java
@@ -5,14 +5,35 @@ import javax.swing.*;
 
 class gamepanel extends JFrame {
   JPanel gameScreen = new JPanel();
+
   public gamepanel() {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(500, 500);
+
+    // Adding Panel to the Screen
+    this.add(gameScreen);
+    gamePanel();
+
+    // Size and Location of Frame
+    this.pack();
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
   }
 
   // Panel Function
   public void gamePanel() {
+    gameScreen.setBackground(Color.black);
+    gameScreen.setPreferredSize(new Dimension(500, 500));
   }
 
   public void gameAlgorithm(String playerChoice) {
+    switch (playerChoice) {
+        case "X":
+          break;
+        case "O":
+          break;
+        default:
+          break;
     }
   }
 }
