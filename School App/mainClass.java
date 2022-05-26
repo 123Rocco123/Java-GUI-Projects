068eb316dc069abd3aaef4af5984504d151b6461
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-26 18:22:44 +0100

    Panel Setup

--- School App/mainClass.java
+++ School App/mainClass.java
@@ -20,5 +20,26 @@ class mainClass extends JFrame {
     this.setVisible(true);
     this.setLayout(null);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    classesPanel.setBounds(5, 10, 250, 200);
+    classesPanel.setBackground(Color.white);
+    this.add(classesPanel);
+
+    alertsPanel.setBounds(265, 10, 250, 200);
+    alertsPanel.setBackground(Color.white);
+    this.add(alertsPanel);
+
+    // X Y WIDTH HEIGHT
+    homeWorkPanel.setBounds(525, 10, 250, 200);
+    homeWorkPanel.setBackground(Color.white);
+    this.add(homeWorkPanel);
+
+    reminderPanel.setBounds(5, 220, 380, 200);
+    reminderPanel.setBackground(Color.white);
+    this.add(reminderPanel);
+
+    calendarPanel.setBounds(395, 220, 380, 200);
+    calendarPanel.setBackground(Color.white);
+    this.add(calendarPanel);
   }
 }
