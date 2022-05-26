8c689de2ac6f24f350e1a4af12ecf57b4152c969
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-26 18:22:06 +0100

    Initial Commit

---
+++ School App/mainClass.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+class mainClass extends JFrame {
+  JPanel classesPanel = new JPanel();
+  JPanel calendarPanel = new JPanel();
+  JPanel reminderPanel = new JPanel();
+  JPanel alertsPanel = new JPanel();
+  JPanel homeWorkPanel = new JPanel();
+
+  JButton exitButton = new JButton("Quit");
+
+  public mainClass() {
+  }
+}
