2c599480dab5e4173cd3088f7fc25823823ab047
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-26 16:14:09 +0100

    Initial Commit

---
+++ School App/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+class Main extends JFrame{
+  public static void main(String[] args) {
+
+  }
+}
---
+++ School App/classes.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+class classes extends JFrame {
+  public classes() {
+    this.setLocationRelativeTo(null);
+  }
+}
