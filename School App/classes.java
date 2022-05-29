ce7e8ad000d3b59cfe74662a0556346a1118fcee
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-29 03:37:26 +0100

    JFrame Change

--- School App/classes.java
+++ School App/classes.java
@@ -3,21 +3,27 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-class classes extends JFrame {
+import java.io.*;
+
+class classes {
+  JFrame testFrame = new JFrame();
+
   JTextField newClass = new JTextField(30);
   JTextField time = new JTextField(30);
 
+  JButton submitButton = new JButton("Save and Close");
+
   public classes() {
-    this.setSize(500, 500);
-    this.setLocationRelativeTo(null);
-    this.setVisible(true);
-    this.setTitle("Add New Class");
-    this.setLayout(null);
+    testFrame.setSize(500, 200);
+    testFrame.setLocationRelativeTo(null);
+    testFrame.setVisible(true);
+    testFrame.setTitle("Add New Class");
+    testFrame.setLayout(null);
 
     newClass.setBounds(10, 10, 465, 50);
     newClass.setText("Enter New Class Name");
     newClass.setHorizontalAlignment(SwingConstants.CENTER);
-    this.add(newClass);
+    testFrame.add(newClass);
 
     time.setBounds(10, 70, 465, 50);
     time.setText("Enter Time of the Class");
