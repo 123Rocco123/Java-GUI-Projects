8100684b51de75291d77a267896045caeab3ec8f
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-05-30 16:56:45 +0100

    JTextFields and Function

--- School App/newHomework.java
+++ School App/newHomework.java
@@ -7,11 +7,40 @@ class newHomework extends JFrame {
 class newHomework {
   JFrame homeworkFrame = new JFrame();
 
+  JTextField newHomework = new JTextField();
+  JTextField homeworkClass = new JTextField();
+
   public newHomework() {
     homeworkFrame.setSize(500, 500);
     homeworkFrame.setLocationRelativeTo(null);
     homeworkFrame.setVisible(true);
     homeworkFrame.setTitle("Add Homework");
     homeworkFrame.setLayout(null);
+
+    homeworkFrame.add(fieldSetterFunc(10, 10, 465, 50, "Enter Homework Name Here", newHomework));
+    homeworkFrame.add(fieldSetterFunc(10, 70, 465, 50, "Enter The Name of the Class For the Homework", homeworkClass));
+
+  public JTextField fieldSetterFunc(int x, int y, int width, int height, String defaultText, JTextField name) {
+    name.setBounds(x, y, width, height);
+    name.setHorizontalAlignment(SwingConstants.CENTER);
+    name.setText(defaultText);
+
+    name.addFocusListener(new FocusListener() {
+      @Override
+      public void focusGained(FocusEvent e) {
+        if (name.getText().equals(defaultText)) {
+          name.setText("");
+        }
+      }
+
+      @Override
+      public void focusLost(FocusEvent e) {
+        if (name.getText().equals("")) {
+          name.setText(defaultText);
+        }
+      }
+    });
+
+    return name;
   }
 }
