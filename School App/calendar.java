70e0f213c9c46fc7c6d8ec7b7dcc3c4339f5394a
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-06-05 18:07:40 +0100

    Calendar and Setup

--- School App/calendar.java
+++ School App/calendar.java
@@ -7,9 +7,19 @@ import javax.swing.*;
 import javax.swing.table.*;
 
 class calendar extends JPanel {
+  Calendar calendar = new GregorianCalendar();
   String[] calendarArray = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
   String[] calendarWeek = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
   public calendar() {
     this.setBounds(10, 35, 360, 155);
+    // JLabel Code
+    calendarTitle = new JLabel(calendarArray[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.YEAR));
+    calendarTitle.setBounds(100, 10, 100, 25);
+    calendarTitle.setHorizontalAlignment(SwingConstants.CENTER);
+    this.add(calendarTitle, BorderLayout.NORTH);
+
+    // First Day of Month
+    calendar.set(Calendar.DAY_OF_MONTH, 1);
+
   }
 }
