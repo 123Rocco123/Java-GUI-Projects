98d130000fe03ec89eeeb34b06cf4be43f46db58
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-06-05 18:07:16 +0100

    Arrays

--- School App/calendar.java
+++ School App/calendar.java
@@ -7,6 +7,8 @@ import javax.swing.*;
 import javax.swing.table.*;
 
 class calendar extends JPanel {
+  String[] calendarArray = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  String[] calendarWeek = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
   public calendar() {
     this.setBounds(10, 35, 360, 155);
   }
