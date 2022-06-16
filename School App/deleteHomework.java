70f51215204626d84e2b93d90c8e5413c2fe62c0
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-06-16 17:26:37 +0100

    JFrame Formatting

--- School App/deleteHomework.java
+++ School App/deleteHomework.java
@@ -4,7 +4,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 class deleteHomework {
+  JFrame homeworkChangeFrame = new JFrame();
+
   public deleteHomework() {
-    
+    homeworkChangeFrame.setTitle("Modify Homework");
+    homeworkChangeFrame.setSize(500, 500);
+    homeworkChangeFrame.setVisible(true);
+    homeworkChangeFrame.setLayout(null);
+    homeworkChangeFrame.setLocationRelativeTo(null);
   }
 }
