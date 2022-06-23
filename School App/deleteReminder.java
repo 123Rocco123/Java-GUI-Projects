947755ed5400c2a7e283e03836e4a0226bfde925
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-06-23 01:09:43 +0100

    Instance Variables

--- School App/deleteReminder.java
+++ School App/deleteReminder.java
@@ -9,6 +9,15 @@ import java.io.*;
 class deleteReminder {
   JFrame deleteReminderFrame = new JFrame();
 
+  // Used to contain the homework names
+  JComboBox homeworkContainer = new JComboBox();
+
+  // Used to contain the homework
+  JTextArea homeworkOutputArea = new JTextArea();
+
+  JButton deleteReminder = new JButton("Delete Reminder");
+  JButton modifyReminder = new JButton("Modify Reminder");
+
   deleteReminder() {
     // JFrame Formatting
     deleteReminderFrame.setTitle("Modify Reminders");
