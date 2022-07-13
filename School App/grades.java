3d4a403edbb84b49e8889ce32c5a1030c579192c
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-07-13 17:10:35 +0100

    JComboBox Function

--- School App/grades.java
+++ School App/grades.java
@@ -24,5 +24,27 @@ class grades {
     gradesFrame.setLayout(null);
 
     gradesFrame.setVisible(true);
+  public void JComboBoxSetup() {
+    // HashMap Setup
+    for (int i = 0; i < new File("./classes").list().length; i++) {
+      int counterToAdd = 0;
+      try {
+        Scanner reader = new Scanner(new File("./classes", new File("./classes").list()[i]));
+
+        // Iterate through the lines in the reader object.
+        while (reader.hasNextLine()) {
+          // Used to not create an inifite loop
+          reader.nextLine();
+
+          counterToAdd += 1;
+        }
+
+        reader.close();
+      } catch (FileNotFoundException ex) {}
+
+      // The ArrayList used for containing the grades for each class.
+      ArrayList<Integer> grades = new ArrayList<Integer>();
+      classGradesMap.put((String)(new File("./classes").list()[i]), grades);
+    }
   }
 }
