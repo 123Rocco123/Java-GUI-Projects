b4dd92b47b592c8ecd7272b5bfa0114d0edc22fe
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-07-13 17:10:21 +0100

    JFrame Formatting

--- School App/grades.java
+++ School App/grades.java
@@ -14,5 +14,15 @@ class grades {
 
   HashMap<String, ArrayList<Integer>> classGradesMap = new HashMap<String, ArrayList<Integer>>();
 
+  public grades() {
+    // JFrame Formatting
+    gradesFrame.setTitle("Grades");
+
+    gradesFrame.setSize(500, 500);
+    gradesFrame.setLocationRelativeTo(null);
+    gradesFrame.setResizable(false);
+    gradesFrame.setLayout(null);
+
+    gradesFrame.setVisible(true);
   }
 }
