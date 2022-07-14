9d1d6883f02e63952fef201c95687ccad2b293a6
Rocco Petruccio <petruccio.rocco@gmail.com>
2022-07-14 20:00:13 +0100

    Code Simplification

--- School App/grades.java
+++ School App/grades.java
@@ -31,21 +31,6 @@ class grades {
   public void JComboBoxSetup() {
     // HashMap Setup
     for (int i = 0; i < new File("./classes").list().length; i++) {
-      int counterToAdd = 0;
-      try {
-        Scanner reader = new Scanner(new File("./classes", new File("./classes").list()[i]));
-
-        // Iterate through the lines in the reader object.
-        while (reader.hasNextLine()) {
-          // Used to not create an inifite loop
-          reader.nextLine();
-
-          counterToAdd += 1;
-        }
-
-        reader.close();
-      } catch (FileNotFoundException ex) {}
-
       // The ArrayList used for containing the grades for each class.
       ArrayList<Integer> grades = new ArrayList<Integer>();
       classGradesMap.put((String)(new File("./classes").list()[i]), grades);
