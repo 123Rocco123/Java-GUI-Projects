c2bcea2c46f3f3f575f12fdb899b5b55386293bf
Rocco Petruccio <63969485+123Rocco123@users.noreply.github.com>
2022-07-22 23:56:31 +0100

    Comments

--- School App/grades.java
+++ School App/grades.java
@@ -78,7 +78,7 @@ class grades {
         if (!gradeContainer.getText().equals("Insert Grade (%)")) {
           // Used to add the grades to HashMap
              // The value variable is used to contain the aggreagte percentage of the grade of a class (REPLACE WITH ARRAYLIST)
-             // We then pass the value of the variable inside of the HashMap so that the equation is stored correctly. 
+             // We then pass the value of the variable inside of the HashMap so that the equation is stored correctly.
           value += Integer.parseInt(gradeContainer.getText());
           classGradesMap.put((String)(chosenClass), value);
 
